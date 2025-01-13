bffe4d3b05302cf46532e05cad1db8f4de8bd0ab
hemantbytes <https://github.com/hemantbytes/React-Native.git>
2025-01-13 11:30:29 +0530

    Reanimated Installation | Animated Flip Card UI

--- App.jsx
+++ App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import MyLogin from './src/components/MyLogin';
 import { SafeAreaView } from 'react-native';
 import "./global.css"
+import AnimatedCardFlip from './src/components/AnimatedCardFlip';
 
 const App = () => {
   return (
-    <SafeAreaView className="flex-1">
-      <MyLogin />
-    </SafeAreaView>
+    <AnimatedCardFlip />
   );
 };
 
