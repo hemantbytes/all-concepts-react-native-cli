6fd2931b2a3d75c79261bb9c9a31d43fc16469da
hemantbytes <https://github.com/hemantbytes/React-Native.git>
2025-01-18 16:18:16 +0530

    How to Create Switch UI Button in React Native

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
-import { View, Text } from 'react-native';
+
+import { View } from 'react-native';
 import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import SwitchUI from './src/components/SwitchUI';
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <View>
-      <Text className="text-center text-3xl">Splash Screen</Text>
+    <View className="flex-1">
+      <SwitchUI />
     </View>
   );
 };
